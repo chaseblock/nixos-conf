cf605ec13175bc3224bf60fc58769299186bde12
Theo Park <theo.park.n@gmail.com>
2026-03-16 00:59:14 -0400

    feat(home-manager): add TexLive packages

--- home-manager/config/misc-pkgs.nix
+++ home-manager/config/misc-pkgs.nix
@@ -3,7 +3,50 @@
   home.packages = with pkgs; [
     tree
     git-filter-repo
-    poppler
+    poppler-utils
+
+    zathuraPkgs.zathura_pdf_poppler
+    (texlive.combine {
+     inherit (pkgs.texlive)
+     scheme-small
+
+     # compilation
+     latexmk
+     luatex
+     synctex
+
+     amsmath
+     amstex
+     amsfonts
+     enumitem
+     minted
+     soul
+     hyperref
+     framed
+     tcolorbox
+     import
+     forest
+
+     # algpseudocodex dependencies (except algorithms)
+     algorithms
+     algpseudocodex
+     algorithmicx
+     fifo-stack
+     varwidth
+     kvoptions
+     etoolbox
+     tabto-ltx
+     totcount
+     tikzmark
+
+     # font related
+     sourcesanspro
+     sourcecodepro
+     sourceserifpro
+     ly1
+     ;
+     })
+
   ];
 }
 
