7371aa9a6e5aa11a6e0cfdc817e52144111b2f65
Theo Park <theo.park.n@gmail.com>
2026-03-16 23:52:37 -0400

    feat(nixos): add initial configuration and hardware-configuration for Framework 13

--- flake.nix
+++ flake.nix
@@ -28,5 +28,17 @@
         }
       ];
     };
+
+    nixosConfigurations.wittgenstein = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/wittgenstein/configuration.nix
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.theopn = ./home-manager/home.nix;
+        }
+      ];
+    };
   };
 }
