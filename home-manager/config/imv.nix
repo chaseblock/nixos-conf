0acec32d401b75041a6e9fe4f6721580c62448c8
Theo Park <theo.park.n@gmail.com>
2026-03-25 16:36:43 -0400

    feat(imv): make the background of overlay text to be noridc theme for readability

--- home-manager/config/imv.nix
+++ home-manager/config/imv.nix
@@ -8,6 +8,8 @@
         background = "#000000";
         loop_input = true;
         overlay = true;   # d to toggle
+        overlay_text_color = "#d8dee9";
+        overlay_background_color = "#4c566a";
         recursively = true;  # use :open -r .
         scaling_mode = "shrink";
       };
