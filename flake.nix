270f037d1322f69b55a63b2757390f98b91e0afb
Chase Block <chase@chaseblock.com>
2026-04-08 20:08:56 -0500

    Add Dell G7 target

--- flake.nix
+++ flake.nix
@@ -22,8 +22,29 @@
     nixosConfigurations.frame = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        #nixos-hardware.nixosModules.framework-amd-ai-300-series
         nixos-hardware.nixosModules.framework-11th-gen-intel
+        ./hosts/frame/configuration.nix
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "hm-bak";
+          home-manager.users.chase = {
+            imports =[
+              nixvim.homeModules.nixvim
+              ./home-manager/home.nix
+              ./home-manager/linux.nix
+            ];
+          };
+        }
+      ];
+    };
+
+    nixosConfigurations.g7 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # This is technically a different laptop, but maybe it will work?
+        nixos-hardware.nixosModules.dell-g3-3779
+        
         ./hosts/frame/configuration.nix
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
